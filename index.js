6e1e24803fd24bd8b673539770fa5c616c2ce1f3
ndavarpanah <79424197+ndavarpanah@users.noreply.github.com>
2025-01-18 01:09:51 -0800

    new readme file

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 class User {
+
+    message;
+    username;
     constructor(){
         this.message = 'Hello World';
         console.log(this.message);
+    }
+
+    setUserName(name){
+        this.username = name;
+    }
+
+    getUserStatus(){
+        return this.username;
     }
 
     getUserName(){}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# My package
+
+A simple node package
+
+## Install Instructions
+
+**run** 
+'npm install @ndavarpanah/my-package@1.1.0'
